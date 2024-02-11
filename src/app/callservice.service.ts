eebc8e5ab4b9d176c8d731220d756ae223d45198
Aniket461 <surveaniket461@gmail.com>
2024-02-11 13:29:02 +0100

    decline and permissions

--- src/app/callservice.service.ts
+++ src/app/callservice.service.ts
@@ -13,13 +13,23 @@ export class CallserviceService {
   stream:any;
   called:Boolean = false;
   mystream:any;
+  myCall:any;
+  videotrack:any
+  connCall:any;
+  audiotrack:any
+  cid:any;
+  
    peer:any;
   connectToPeer(myid:string,callerid:string){
     this.conn = this.peer.connect(callerid);
+    this.cid = callerid;
     console.log(this.conn);
     this.conn.on('open',()=>{
      this.conn.send("Hello from"+myid);
     }) 
+    this.conn.on('close',()=>{
+      window.location.href = "/userlist";
+    })
   }
 
   openconnection(myid:string){
@@ -31,11 +41,27 @@ export class CallserviceService {
 
   sendMessages(message:string){
     this.conn.send(message);
+    
   }
 
+  closeCall(){
+    console.log(this.peer.connections);
+    for (let conns in this.peer.connections) {
+      this.peer.connections[conns].forEach((conn:any) => {
+        conn.peerConnection.close();
+        // close it using peerjs methods
+        if (conn.close) {
+            conn.close();
+        }
+      })}
+      this.mystream.getTracks().forEach((track: { stop: () => any; }) => track.stop())
+      this.stream.getTracks().forEach((track: { stop: () => any; }) => track.stop())
+      window.location.href = "/userlist"
+  }
   recieveCall(){
     if(this.peer != undefined){
     this.peer.on("connection", (conn:any) => {
+      this.connCall = conn;
       conn.on("data", (data:any) => {
         // Will print 'hi!'
         console.log(data);
@@ -43,30 +69,61 @@ export class CallserviceService {
       conn.on("open", () => {
         conn.send("hello!");
       });
+      conn.on("close",()=>{
+        window.location.href = '/userlist';
+      })
     })
   }
   }
   async RecieveCall(){
+
+
     this.peer.on("call", async (call:any) => {
+      var acceptCall = confirm(localStorage.getItem("name")+"is calling you, Accept?");
+      if(acceptCall){
+  
       let stream = await navigator.mediaDevices.getUserMedia(
         { video: true, audio: true })
-        
+        this.myCall = call;
                  this.mystream = stream;
-          call.answer(stream); // Answer the call with an A/V stream.
+                 this.called = true;
+                 this.videotrack = stream.getVideoTracks()[0];
+                 this.audiotrack = stream.getAudioTracks()[0];
+          call.answer(stream);
+
+          call.on('close',()=>{
+            window.location.href = "/userlist"
+          })
+          
+          call.on('error',()=>{
+            alert("error");
+          })
+          // Answer the call with an A/V stream.
           call.on("stream", (remoteStream:any) => {
             console.log(remoteStream);
             this.stream = remoteStream;
             this.called = true;
         })
+      }
+      else{
+        alert("Call Declined");
+      }
     });
   }
-
   async VideoCall(callerid:string){
     const stream = await navigator.mediaDevices.getUserMedia(
       { video: true, audio: true });
 this.mystream = stream;
+this.videotrack = stream.getVideoTracks()[0];
+this.audiotrack = stream.getAudioTracks()[0];
 console.log(this.mystream);
         const call = this.peer.call(callerid,stream);
+        call.on('close',()=>{
+          window.location.href = "/userlist"
+        })
+        call.on('error',()=>{
+          alert("error");
+        })
         call.on("stream", (remoteStream:any) => {
           this.stream = remoteStream;
           this.RecieveCall();
--- src/app/videocall/videocall.component.ts
+++ src/app/videocall/videocall.component.ts
@@ -14,6 +14,47 @@ export class VideocallComponent implements OnInit {
   }
 
 
+  videoOn:Boolean = true;
+audioOn :Boolean = true;
+
+
+
+  removeVideo(){
+    this.callservice.mystream.removeTrack(this.callservice.videotrack);
+    (<HTMLVideoElement>document.getElementById('mystream')).srcObject = null;
+    this.videoOn = false;
+  }
+
+  addVideo(){
+    this.callservice.mystream.addTrack(this.callservice.videotrack);
+    (<HTMLVideoElement>document.getElementById('mystream')).srcObject = this.callservice.mystream;
+    this.videoOn = true;
+  }
+
+  ToggleAudio(){
+    if(this.audioOn){
+      this.removeAudio();
+      this.audioOn = !this.audioOn
+    }
+    else{
+      this.addAudio();
+      this.audioOn = !this.audioOn
+    }
+  }
+  
+  removeAudio(){
+    console.log(this.callservice.mystream.getVideoTracks())
+    this.callservice.mystream.removeTrack(this.callservice.audiotrack);
+    }
+
+  addAudio(){
+    this.callservice.mystream.addTrack(this.callservice.audiotrack);
+     }
+
+     closeCall(){
+      this.callservice.closeCall();
+     }
+
   ngOnInit(): void {
   }
 
