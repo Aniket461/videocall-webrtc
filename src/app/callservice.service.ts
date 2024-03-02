e46705d2d8b2b400f0df31061e766438a310d6a7
Aniket461 <surveaniket461@gmail.com>
2024-03-02 07:02:54 +0100

    UI Enchancement

--- src/app/callservice.service.ts
+++ src/app/callservice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {DataConnection, Peer} from 'peerjs';
+import { DataConnection, Peer } from 'peerjs';
 import { UserlistComponent } from './userlist/userlist.component';
 import { UserserviceService } from './userservice.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -7,254 +7,266 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogueboxComponent } from './dialoguebox/dialoguebox.component';
 import { firstValueFrom } from 'rxjs';
 import { BasicpopupComponent } from './basicpopup/basicpopup.component';
+import { Howl, Howler } from 'howler';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CallserviceService {
+  constructor(public snackbar: MatSnackBar, private dialog: MatDialog) {}
 
-  constructor(public snackbar:MatSnackBar,private dialog: MatDialog) { }
-
-
-  conn:any;
-  icalled:boolean = false;
-  conn2:any;
-  stream:any;
-  called:Boolean = false;
-  mystream:any;
-  myCall:any;
-  videotrack:any
-  connCall:any;
-  audiotrack:any
-  cid:any;
-  messages:any = [];
-   peer:any;
-   callrecieved = false;
-   dialogclicked = false;
-   public answer = false;
-  async connectToPeer(myid:string,callerid:string){
+  sound: any;
+  conn: any;
+  icalled: boolean = false;
+  conn2: any;
+  stream: any;
+  called: Boolean = false;
+  mystream: any;
+  myCall: any;
+  videotrack: any;
+  connCall: any;
+  audiotrack: any;
+  cid: any;
+  messages: any = [];
+  peer: any;
+  callrecieved = false;
+  dialogclicked = false;
+  public answer = false;
+  async connectToPeer(myid: string, callerid: string) {
     this.conn = await this.peer.connect(callerid);
     console.log(this.conn);
-    if(this.conn != undefined){
-    this.cid = callerid;
+    if (this.conn != undefined) {
+      this.cid = callerid;
 
-    this.conn.on('open',()=>{
+      this.conn.on('open', () => {});
+      this.conn.on('data', (data: any) => {
+        console.log(data);
 
-    }) ;
-    this.conn.on('data',(data:any)=>{
+        if (data == 'irecieve') {
+          this.callrecieved = true;
+          this.sound.stop();
+        } else if (data == 'No call') {
+          this.sound.stop();
+          this.closeCall();
+        } else {
+          this.messages.push({ id: 'reciever', message: data });
+        }
+      });
 
-      console.log(data);
-      
-      if(data == "irecieve"){
-        this.callrecieved = true;
-      }
-      else if(data == "No call"){
-        this.closeCall();
-      }
-      else{
-      this.messages.push({"id":"reciever","message":data});
-      }
-      
-    })
+      this.conn.on('close', () => {
+        window.location.href = '/userlist';
+      });
 
-    this.conn.on('close',()=>{
-      window.location.href = "/userlist";
-    });
+      console.log(this.conn.dataChannel.readyState);
 
-    console.log(this.conn.dataChannel.readyState);
+      setTimeout(() => {
+        if (!this.callrecieved) {
+          this.sound.stop();
+          this.basicpopup('Call was not answered, redirecting to call list!!');
+        }
+      }, 15000);
+      // if(this.conn.open){
 
+      //   alert('The user is offline, Call cannot be placed!');
+      //   window.location.href = '/userlist';
 
-    setTimeout(()=>{
-      if(!this.callrecieved){
-        this.basicpopup("Call was not answered, redirecting to call list!!");
-      }
-    },15000)
-    // if(this.conn.open){
-
-    //   alert('The user is offline, Call cannot be placed!');
-    //   window.location.href = '/userlist';
-
-    // }
+      // }
+    } else {
+      console.log('In else');
+      const dialogref = this.dialog.open(BasicpopupComponent, {
+        data: { message: 'User is not online right now!, call cannot be placed...', isfuncPresent: false },
+      });
+      dialogref.afterOpened().subscribe((res) => {
+        setTimeout(() => {
+          window.location.href = '/userlist';
+        }, 2000);
+      });
+    }
   }
-  else{
-    console.log("In else");
-    const dialogref = this.dialog.open(DialogueboxComponent,{data: { message: "User is not online right now!, Call cannot be placed" }});
-  }
-      }
 
-  openconnection(myid:string){
-    this.peer= new Peer(myid);
+  openconnection(myid: string) {
+    this.peer = new Peer(myid);
     this.recieveCall();
     this.RecieveCall();
   }
 
-  sendMessages(message:string){
+  sendMessages(message: string) {
     this.conn.send(message);
   }
 
-  muteAudio(from:boolean = false){
-
-
+  muteAudio(from: boolean = false) {
     for (let conns in this.peer.connections) {
-      this.peer.connections[conns].forEach((conn:any) => {
-    console.log(conn.peerConnection.getSenders())
+      this.peer.connections[conns].forEach((conn: any) => {
+        console.log(conn.peerConnection.getSenders());
         const sender = conn.peerConnection.getSenders();
 
-        sender[1] != undefined ?sender[1].replaceTrack(this.mystream.getVideoTracks()[0]):null;
-        sender[0] != undefined ?sender[0].replaceTrack(this.mystream.getAudioTracks()[0]):null;
-      })}
+        sender[1] != undefined
+          ? sender[1].replaceTrack(this.mystream.getVideoTracks()[0])
+          : null;
+        sender[0] != undefined
+          ? sender[0].replaceTrack(this.mystream.getAudioTracks()[0])
+          : null;
+      });
+    }
   }
 
-  closeCall(){
+  closeCall() {
     for (let conns in this.peer.connections) {
-      this.peer.connections[conns].forEach((conn:any) => {
+      this.peer.connections[conns].forEach((conn: any) => {
         conn.peerConnection.close();
         // close it using peerjs methods
         if (conn.close) {
-            conn.close();
+          conn.close();
         }
-      })}
-      this.mystream != undefined ?this.mystream.getTracks().forEach((track: { stop: () => any; }) => track.stop()):null
-      this.stream != undefined ?this.stream.getTracks().forEach((track: { stop: () => any; }) => track.stop()):null
-      window.location.href = "/userlist"
-    
+      });
+    }
+    this.mystream != undefined
+      ? this.mystream
+          .getTracks()
+          .forEach((track: { stop: () => any }) => track.stop())
+      : null;
+    this.stream != undefined
+      ? this.stream
+          .getTracks()
+          .forEach((track: { stop: () => any }) => track.stop())
+      : null;
+    window.location.href = '/userlist';
   }
 
-  
-  
-  async basicpopup(message:string){
-    
-    const dialogref = this.dialog.open(BasicpopupComponent,{data: { message: message ,isfuncPresent:false }});
-    dialogref.afterOpened().subscribe(res =>{
-      setTimeout(()=>{
-        window.location.href = "/userlist";
-      },2000)
-    })
-   
+  async basicpopup(message: string) {
+    const dialogref = this.dialog.open(BasicpopupComponent, {
+      data: { message: message, isfuncPresent: false },
+    });
+    dialogref.afterOpened().subscribe((res) => {
+      setTimeout(() => {
+        window.location.href = '/userlist';
+      }, 2000);
+    });
   }
 
-
-  async openDialog(message:string){
-    
+  async openDialog(message: string) {
     this.dialogclicked = false;
     this.answer = false;
-    const dialogref = this.dialog.open(DialogueboxComponent,{data: { message: message }})
+    const dialogref = this.dialog.open(DialogueboxComponent, {
+      data: { message: message },
+    });
     await firstValueFrom(dialogref.afterClosed());
   }
 
-
-  recieveCall(){
-    this.peer.on("connection", (conn:any) => {
+  recieveCall() {
+    this.peer.on('connection', (conn: any) => {
       this.connCall = conn;
       this.conn2 = conn;
-      conn.on("data", (data:any) => {
-      
-        this.messages.push({"id":"reciever","message":data});
+      conn.on('data', (data: any) => {
+        this.messages.push({ id: 'reciever', message: data });
 
-
-        if(data == "No call"){
+        if (data == 'No call') {
           this.closeCall();
-        }        
+        }
       });
-      conn.on("open", () => {
+      conn.on('open', () => {});
 
-      });
-
-      conn.on('error',(err:any)=>{
-
+      conn.on('error', (err: any) => {
         console.log(err);
-      })
-      conn.on("close",()=>{
+      });
+      conn.on('close', () => {
         window.location.href = '/userlist';
-      })
-    })
+      });
+    });
   }
-  async RecieveCall(){
-    this.peer.on("call", async (call:any) => {
-      call.on('close',()=>{
-        window.location.href = "/userlist"
-      })
-      
-      await this.openDialog("Would you like to to answer??");
-      console.log(this.answer);
-      
+  async RecieveCall() {
+    this.peer.on('call', async (call: any) => {
+      call.on('close', () => {
+        window.location.href = '/userlist';
+      });
+      this.sound = new Howl({
+        src: ['../assets/teams_next_level.mp3'],
+        loop: true,
+      });
 
-      
-      if(this.answer){
-    this.icalled = false;
-      let stream = await navigator.mediaDevices.getUserMedia(
-        { video: true, audio: true })
+      this.sound.play();
+      await this.openDialog('Would you like to to answer??');
+      console.log(this.answer);
+
+      this.sound.stop();
+
+      if (this.answer) {
+        this.icalled = false;
+        let stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
         this.myCall = call;
-                 this.mystream = stream;
-                 this.called = true;
-                 this.videotrack = stream.getVideoTracks()[0];
-                 this.audiotrack = stream.getAudioTracks()[0];
-          call.answer(stream);
-          this.conn2.send('irecieve')
-          call.on('close',()=>{
-            window.location.href = "/userlist"
-          })
-          
-          call.on('error',()=>{
-            alert("error");
-          })
-          // Answer the call with an A/V stream.
-          call.on("stream", (remoteStream:any) => {
-            
-            this.stream = remoteStream;
-            this.called = true;
-        })
-      }
-      else{
-        this.conn2.send("No call");
+        this.mystream = stream;
+        this.called = true;
+        this.videotrack = stream.getVideoTracks()[0];
+        this.audiotrack = stream.getAudioTracks()[0];
+        call.answer(stream);
+        this.conn2.send('irecieve');
+        call.on('close', () => {
+          window.location.href = '/userlist';
+        });
+
+        call.on('error', () => {
+          alert('error');
+        });
+        // Answer the call with an A/V stream.
+        call.on('stream', (remoteStream: any) => {
+          this.stream = remoteStream;
+          this.called = true;
+        });
+      } else {
+        this.conn2.send('No call');
         call.close();
         this.closeCall();
       }
+    });
+  }
 
+  sendMessagetoReciever(message: string) {
+    this.conn.send(message);
+    this.messages.push({ id: 'sender', message: message });
+  }
+  sendMessagesToCaller(message: string) {
+    this.conn2.send(message);
+    this.messages.push({ id: 'sender', message: message });
+  }
 
+  async VideoCall(callerid: string, myid: string) {
+    this.sound = new Howl({
+      src: ['../assets/teams_bop.mp3'],
+      loop: true,
     });
 
-  
-  }
+    this.sound.play();
 
-sendMessagetoReciever(message:string){
-  this.conn.send(message);
-  this.messages.push({"id":"sender","message":message})
-}
-  sendMessagesToCaller(message:string){
-    this.conn2.send(message)
-    this.messages.push({"id":"sender","message":message})
-  }
-
-
-  async VideoCall(callerid:string, myid:string){
-    
-    await this.connectToPeer(myid,callerid);
+    await this.connectToPeer(myid, callerid);
     this.icalled = true;
     console.log(this.conn);
-    if(this.conn != undefined){
-    const stream = await navigator.mediaDevices.getUserMedia(
-      { video: true, audio: true });
-this.mystream = stream;
+    if (this.conn != undefined) {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      this.mystream = stream;
 
-this.called = true;
-this.videotrack = stream.getVideoTracks()[0];
-this.audiotrack = stream.getAudioTracks()[0];
-        const call = await this.peer.call(callerid,stream);
-        console.log(call);
-          call.on("stream", (remoteStream:any) => {
-            this.stream = remoteStream;
-            this.RecieveCall();
-            this.called = true;
-          });
-          call.on('close',()=>{
-            window.location.href = '/userlist';
-          })
+      this.called = true;
+      this.videotrack = stream.getVideoTracks()[0];
+      this.audiotrack = stream.getAudioTracks()[0];
+      const call = await this.peer.call(callerid, stream);
+      console.log(call);
+      call.on('stream', (remoteStream: any) => {
+        this.stream = remoteStream;
+        this.RecieveCall();
+        this.called = true;
+      });
+      call.on('close', () => {
+        window.location.href = '/userlist';
+      });
 
-          call.on('error',(err:any)=>{
-            console.log(err);
-          })
-      }
-
+      call.on('error', (err: any) => {
+        console.log(err);
+      });
     }
-
+  }
 }
